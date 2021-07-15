7c64e657c0ddeb0c3acac1e0d262cb92dc4f6e0f
Lana Ferrari <ferrarirangel@gmail.com>
2021-07-15 20:59:12 +0100

    Changes 2

--- index.js
+++ index.js
@@ -16,30 +16,42 @@ let address = users[0].address.city;
 let message = "";
 
 for (let i = 0; i < users.length; i++) {
-const id = users[i].id;
-const name = users[i].name;
-const city= users[i].address.city;
+const id = "User ID: " + users[i].id;
+const name = "Name: " + users[i].name;
+const city= "City: " + users[i].address.city;
 
-const row = `${id} || ${name} || ${city} \n`;
+const row = `${id} | ${name} | ${city} \n`;
 
 message = message + row;
 
-console.log(message)
+//console.log(message)
 }
 
  alert(message);
 
- console.log(users[0].id , users[0].name , users[0].address.city);
+ //console.log(users[0].id , users[0].name , users[0].address.city);
 
  //- Prompt the user for a user id
 
- let userId = prompt("Who's ID do you wanna check? Use and ID number.");
+ let userId = prompt("Who's ID do you wanna check? Use an ID number.");
 
 //- Display an alert with the username and all the todos titles that belong to that user 
 
- const array = []
 
-for (let i = 0; i < users.length; i++) {
-  const element = users[i]
-  const todos = 
+let messageAlert ="";
+
+for (let i = 0; i < todos.length; i++) {
+const todosUserId = todos[i].userId;
+const todosId = "ID: " + todos[i].id;
+const todosTitleTasks = "Tittle: " + todos[i].title;
+const todosCompletedTasks = "Completed: " + todos[i].completed;
+
+const row = `${todosUserId} || ${todosId} || ${todosTitleTasks}||  ${todosCompletedTasks} \n`;
+
+if(todosUserId == userId){
+messageAlert = messageAlert + row;
+}else{
+  messageAlert = "Try again!";
 }
+}
+ alert(messageAlert);
